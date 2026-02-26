7c07b63c09640ef395a29ffaf3f34e7f279bd3b6
Magestick <74025726+Magest1ckkz@users.noreply.github.com>
2026-02-26 10:24:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function copyText(){
+    navigator.clipboard.writeText(`<a href="https://magest1ckkz.github.io/">\n<img src="https://raw.githubusercontent.com/Magest1ckkz/magest1ckkz.github.io/refs/heads/main/mage-webring.png" width="96px" alt="mage_webring">\n</a>`)
+    }
