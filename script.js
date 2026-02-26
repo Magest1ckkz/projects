11cea30e37ae5d61982a9496c54d5c30b8ac2fc6
Magestick <74025726+Magest1ckkz@users.noreply.github.com>
2026-02-26 16:36:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function copyText(){
-    navigator.clipboard.writeText(`<a href="https://magest1ckkz.github.io/">\n<img src="https://raw.githubusercontent.com/Magest1ckkz/magest1ckkz.github.io/refs/heads/main/mage-webring.png" width="96px" alt="mage_webring">\n</a>`)
+    navigator.clipboard.writeText(`<a href="https://magest1ckkz.github.io/">\n<img src="https://raw.githubusercontent.com/Magest1ckkz/magest1ckkz.github.io/refs/heads/main/mage-webring.png" width="104px" height="32px" alt="mage_webring">\n</a>`)
+
     }
